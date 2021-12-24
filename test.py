94e3ed559581267d983a932e832167c39ef996ed
DmitriyKokin <60880186+DmitriyKokin@users.noreply.github.com>
2021-12-24 17:08:49 +0300

    перевод чисел в 2 систему счисления, написанный на python с помощью kivy

---
+++ test.py
@@ -0,0 +1,60 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+ 
+class MainApp(App):
+    def build(self):
+        self.last_button = ''
+        main_layout = BoxLayout(orientation="vertical")
+        self.solution = TextInput(
+            multiline=False, readonly=True, halign="right", font_size=50
+        )
+        main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9"],
+            ["4", "5", "6"],
+            ["1", "2", "3"],
+            ["0", "C"],
+        ]
+        for row in buttons:
+            h_layout = BoxLayout()
+            for label in row:
+                button = Button(
+                    text=label,
+                    pos_hint={"center_x": 0.5, "center_y": 0.5},
+                )
+                button.bind(on_press=self.on_button_press)
+                h_layout.add_widget(button)
+            main_layout.add_widget(h_layout)
+ 
+        equals_button = Button(
+            text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}
+        )
+        equals_button.bind(on_press=self.on_solution)
+        main_layout.add_widget(equals_button)
+        return main_layout
+ 
+    def on_button_press(self, instance):
+        current = self.solution.text
+        button_text = instance.text
+        if button_text == "C":
+            self.solution.text = ""
+        else:
+                if (self.last_button == '='):
+                    current = ''
+                    self.last_button = ''
+                new_text = current + button_text
+                self.solution.text = new_text
+        self.last_button = button_text
+ 
+    def on_solution(self, instance):
+        text = self.solution.text
+        self.last_button = '='
+        if text:
+            solution = str(bin(int(self.solution.text)))
+            self.solution.text = solution[2:]
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
